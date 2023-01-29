348af50fcfab274ec88b1caab9834728431cf238
Hedyenee <hedyen.mili@gmail.com>
2023-01-29 18:53:47 +0100

    second

--- routes/view.js
+++ routes/view.js
@@ -1,15 +1,36 @@
-const express = require ('express')
-const router = express.Router()
-const path = require ('path')
+// const express = require ('express')
+// const router = express.Router()
+// const path = require ('path')
+// const app = express()
 
-router.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname,'../','public','home.html'))
-})
-router.get('/ourService',(req,res)=>{
-    res.sendFile(path.join(__dirname,'../','public','ourService.html'))
-})
-router.get('/contact',(req,res)=>{
-    res.sendFile(path.join(__dirname,'../','public','contact.html'))
-})
+// /* app.use(express.static(__dirname+'/public')) */
 
-module.exports=router
+// const requestTime = function (req, res, next) {
+//     const today =new Date();
+//    hours = today.getHours()
+//    day = today.getDay()
+
+//     if ( ((hours>=13)&&(hours<17)) && ((day>=1) &&(day<=5))){
+//         console.log("open")
+//         next()   
+//   }
+//   else{
+//     res.send('closed')
+//   }
+// }
+
+
+  
+
+
+// router.get('/',requestTime,(req,res)=>{
+//     res.sendFile(path.join(__dirname,'../','public','home.html'))
+// })
+// router.get('/ourService',requestTime,(req,res)=>{
+//     res.sendFile(path.join(__dirname,'../','public','ourService.html'))
+// })
+// router.get('/contact',requestTime,(req,res)=>{
+//     res.sendFile(path.join(__dirname,'../','public','contact.html'))
+// })
+
+// module.exports=router
--- server.js
+++ server.js
@@ -1,10 +1,28 @@
 const express = require ('express')
 const app = express()
-app.use(express.static(__dirname+'/public'))
+/* app.use(express.static(__dirname+'/public')) */
+
+const requestTime = function (req, res, next) {
+    const today =new Date(Date.now());
+   hours = today.getHours()  
+   day = today.getDay()
+
+    if ( ((hours>=9)&&(hours<20)) ){
+        console.log("open")
+        next()   
+  }
+  else{
+    res.send('closed')
+  }
+}
 
 
 
-app.use('/views',require('./routes/view'))
+
+app.use(requestTime)
+app.use(express.static('public'))
+
+
 app.listen(5000,(err)=> {
     if (err) throw err 
     else console.log('server is running on port 5000')
